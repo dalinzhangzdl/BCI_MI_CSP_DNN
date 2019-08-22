626d9c2e57817ce47e9acc445f66d576822f3ef4
zhangdalin <40844084+dalinzhangzdl@users.noreply.github.com>
2019-08-22 10:56:53 +0800

    Update label_processing.m

--- label_processing.m
+++ label_processing.m
@@ -1,16 +1,16 @@
 clear all
 clc
-Data = load('graz_data\dataset_BCIcomp1.mat'); % 加载训练数据集
-load('graz_data\labels_data_set_iii.mat');   % 加载测试数据集标签 y_test
+Data = load('graz_data\dataset_BCIcomp1.mat'); % load train data
+load('graz_data\labels_data_set_iii.mat');   % load test label  y_test
 trian_label = Data.y_train;
 test_label = y_test;
 s = length(trian_label);
 train_output = zeros(s,2);
 test_output = zeros(s,2);
 for i = 1:s
-    train_output(i,trian_label(i)) = 1; % label_data(i)的值为1或2
-    test_output(i,test_label(i)) = 1;   % 同上
+    train_output(i,trian_label(i)) = 1; 
+    test_output(i,test_label(i)) = 1;   
 end
 Train_label = train_output';
 Test_label = test_output';
-save('label.mat', 'Train_label','Test_label');
+save('label.mat', 'Train_label','Test_label');  % save label 
